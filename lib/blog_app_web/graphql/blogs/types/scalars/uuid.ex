dea5683e467f7003a096a4f2644075862523909b
getty104 <hayabusatoshihumi@gmail.com>
2019-01-03 18:20:33 +0900

    Fix scalar's error handling for returning GraphQL result data

--- lib/blog_app_web/graphql/blogs/types/scalars/uuid.ex
+++ lib/blog_app_web/graphql/blogs/types/scalars/uuid.ex
@@ -4,26 +4,34 @@ defmodule BlogAppWeb.Blogs.Scalars.UUID do
       scalar :uuid do
         description("UUID")
 
-        parse(
-          &{:ok,
-           &1.value
+        parse(&parse_value/1)
+
+        serialize(&serialize_value/1)
+      end
+
+      defp parse_value(data) do
+        try do
+          {:ok,
+           data.value
            |> Base.decode16!(case: :lower)
            |> Base.hex_decode32!(case: :lower)
            |> Base.decode64!()
            |> String.to_integer(31)
            |> :math.pow(0.5)
            |> round}
-        )
+        rescue
+          e -> :error
+        end
+      end
 
-        serialize(
-          &(&1
-            |> :math.pow(2)
-            |> round
-            |> Integer.to_string(31)
-            |> Base.encode64()
-            |> Base.hex_encode32(case: :lower)
-            |> Base.encode16(case: :lower))
-        )
+      defp serialize_value(value) do
+        value
+        |> :math.pow(2)
+        |> round
+        |> Integer.to_string(31)
+        |> Base.encode64()
+        |> Base.hex_encode32(case: :lower)
+        |> Base.encode16(case: :lower)
       end
     end
   end
